75263c5cb9a263094c4ed4cf1e192ecbb3286069
sreylenchab <126034400+CHAB-Sreylen@users.noreply.github.com>
2023-03-23 18:41:50 +0700

    Update hello.java

--- hi/hello.java
+++ hi/hello.java
@@ -3,5 +3,6 @@ package hi;
 public class hello {
     public static void main(String[] args) {
         System.out.println("Welcome My name is Reaksmey");
+        System.out.println("I am CHAB Sreylen");
     }
 }
