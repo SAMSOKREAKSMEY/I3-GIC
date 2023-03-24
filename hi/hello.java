6bfe7aa9d6b13be7e5982a88cdcc1be7e46a6596
SAMSOKREAKSMEY <127179736+SAMSOKREAKSMEY@users.noreply.github.com>
2023-03-24 19:14:10 +0700

    Delete hi directory

--- hi/hello.java
+++
@@ -1,8 +0,0 @@
-package hi;
-
-public class hello {
-    public static void main(String[] args) {
-        System.out.println("Welcome My name is Reaksmey");
-        System.out.println("I am CHAB Sreylen");
-    }
-}
